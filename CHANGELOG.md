2a1c014f8ee6c5d37d6432096aaec9f2779d1894
omrilotan <omrilotan@hotmail.com>
2022-10-17 09:08:28 +0100

    Update error dictionary

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,34 +1,41 @@
-# 1.1.4 2022-09-19
-## Update error dictionary
+# CHANGELOG
+
+## 1.1.5 2022-10-17
+
+### Update error dictionary
+- Add: 441-455
+
+## 1.1.4 2022-09-19
+### Update error dictionary
 - Add: 436-440
 
-# 1.1.3 2022-07-27
-## Update error dictionary
+## 1.1.3 2022-07-27
+### Update error dictionary
 - Add: 434, 435
 
-# 1.1.2 2022-06-07
-## Update error dictionary
+## 1.1.2 2022-06-07
+### Update error dictionary
 - Add: 432, 433
 
-# 1.1.1 2022-03-17
-## Update error dictionary
+## 1.1.1 2022-03-17
+### Update error dictionary
 - Modify: 355, 418
 - Add: 419-431
 
-# 1.1.0 2022-01-10
-## Add ability to get error details: url and invariant number
+## 1.1.0 2022-01-10
+### Add ability to get error details: url and invariant number
 ```js
 decode.details(...)
 ```
 
-## Update error dictionary
+### Update error dictionary
 - Add: 418
 
-# 1.0.0 2021-10-12
-## Update error dictionary
+## 1.0.0 2021-10-12
+### Update error dictionary
 - Remove: 291, 296
 - Update: 350, 365
 - Add: 385-417
 
-# 0.1.0 2021-01-18
-## Decode React minified error message
+## 0.1.0 2021-01-18
+### Decode React minified error message
--- spec.js
+++ spec.js
@@ -1,21 +1,9 @@
 const decode = require('.');
 
 describe('react-error-decoder', () => {
-	it.each([
-		[
-			'Minified React error #130; visit https://reactjs.org/docs/error-decoder.html?invariant=130&args[]=undefined&args[]= for the full message or use the non-minified dev environment for full errors and additional helpful warnings.',
-			'Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: undefined.',
-		],
-	])('decodes messages successfully', (encoded, decoded) => {
-		expect(decode(encoded)).toBe(decoded);
-	});
-
-	it.each([
-		[
-			'Minified React error #130; visit https://reactjs.org/docs/error-decoder.html?invariant=130&args[]=undefined&args[]= for the full message or use the non-minified dev environment for full errors and additional helpful warnings.',
-			'Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: undefined.',
-		],
-	])('get all details for error', (encoded, decoded) => {
+	it('get all details for error', () => {
+		const encoded = 'Minified React error #130; visit https://reactjs.org/docs/error-decoder.html?invariant=130&args[]=undefined&args[]= for the full message or use the non-minified dev environment for full errors and additional helpful warnings.';
+		const decoded = 'Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: undefined.';
 		expect(decode.details(encoded)).toEqual({
 			message: decoded,
 			url: 'https://reactjs.org/docs/error-decoder.html?invariant=130&args[]=undefined&args[]=',
@@ -24,12 +12,23 @@ describe('react-error-decoder', () => {
 	});
 	it.each([
 		[
+			'Minified React error #130; visit https://reactjs.org/docs/error-decoder.html?invariant=130&args[]=undefined&args[]= for the full message or use the non-minified dev environment for full errors and additional helpful warnings.',
+			'Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: undefined.',
+		],
+		[
+			'Minified React error #152; visit https://reactjs.org/docs/error-decoder.html?invariant=152&args[]=NGABuilder for the full message or use the non-minified dev environment for full errors and additional helpful warnings.',
+			'NGABuilder(...): Nothing was returned from render. This usually means a return statement is missing. Or, to render nothing, return null.',
+		],
+	])('decodes messages successfully', (encoded, decoded) => {
+		expect(decode(encoded)).toBe(decoded);
+	});
+	it.each([
+		[
 			'Minified React error #223; visit https://reactjs.org/docs/error-decoder.html?invariant=223&args[]=undefined&args[]= for the full message or use the non-minified dev environment for full errors and additional helpful warnings.',
 		],
 	])('Leave an error not in dictionary as is', encoded => {
 		expect(decode(encoded)).toBe(encoded);
 	});
-
 	it('creates an object from regular error messages', () => {
 		expect(decode.details('Something must have gone horribly wrong')).toEqual({
 			message: 'Something must have gone horribly wrong',
@@ -37,7 +36,6 @@ describe('react-error-decoder', () => {
 			invariant: undefined,
 		});
 	});
-
 	it.each([
 		'visit https://reactjs.org/docs/error-decoder.html?invariant=130&args[]=undefined&args[]= for the full message',
 		'Minified React error #1: Something else has gone wrong.',
